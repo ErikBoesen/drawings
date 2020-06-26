0ec85d13277abef9aad7f626a8ded1be28c0f7cf
ErikBoesen <me@erikboesen.com>
2020-06-26 17:53:08 -0400

    Add dict of ops

--- script.js
+++ script.js
@@ -8,44 +8,46 @@ ctx.strokeStyle = 'black';
 ctx.lineWidth = 1;
 currentPath = null;
 
-const PADDING = 10;
-const DRAWING_SIZE = 800;
-const NUM_STROKES = 30;
-const GAP = 20;
+let ops = {
+    padding: 10,
+    size: 800,
+    strokeCount: 30,
+    strokeSpacing: 20
+}
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
 
     ctx.moveTo(0, 0);
-    ctx.lineTo(DRAWING_SIZE, 0);
-    ctx.lineTo(DRAWING_SIZE, DRAWING_SIZE);
-    ctx.lineTo(0, DRAWING_SIZE);
+    ctx.lineTo(ops.size, 0);
+    ctx.lineTo(ops.size, ops.size);
+    ctx.lineTo(0, ops.size);
     ctx.lineTo(0, 0);
     ctx.stroke();
 
     // Top left
-    for (let i = 1; i <= NUM_STROKES; i++) {
-        ctx.moveTo(0, i * GAP);
-        ctx.lineTo((NUM_STROKES - i) * GAP, 0);
+    for (let i = 1; i <= ops.strokeCount; i++) {
+        ctx.moveTo(0, i * ops.strokeSpacing);
+        ctx.lineTo((ops.strokeCount - i) * ops.strokeSpacing, 0);
         ctx.stroke();
     }
     // Top right
-    for (let i = 1; i <= NUM_STROKES; i++) {
-        ctx.moveTo(DRAWING_SIZE, i * GAP);
-        ctx.lineTo(DRAWING_SIZE - ((NUM_STROKES - i) * GAP), 0);
+    for (let i = 1; i <= ops.strokeCount; i++) {
+        ctx.moveTo(ops.size, i * ops.strokeSpacing);
+        ctx.lineTo(ops.size - ((ops.strokeCount - i) * ops.strokeSpacing), 0);
         ctx.stroke();
     }
     // Bottom left
-    for (let i = 1; i <= NUM_STROKES; i++) {
-        ctx.moveTo(0, DRAWING_SIZE - (i * GAP));
-        ctx.lineTo((NUM_STROKES - i) * GAP, DRAWING_SIZE);
+    for (let i = 1; i <= ops.strokeCount; i++) {
+        ctx.moveTo(0, ops.size - (i * ops.strokeSpacing));
+        ctx.lineTo((ops.strokeCount - i) * ops.strokeSpacing, ops.size);
         ctx.stroke();
     }
     // Bottom right
-    for (let i = 1; i <= NUM_STROKES; i++) {
-        ctx.moveTo(DRAWING_SIZE, DRAWING_SIZE - (i * GAP));
-        ctx.lineTo(DRAWING_SIZE - ((NUM_STROKES - i) * GAP), DRAWING_SIZE);
+    for (let i = 1; i <= ops.strokeCount; i++) {
+        ctx.moveTo(ops.size, ops.size - (i * ops.strokeSpacing));
+        ctx.lineTo(ops.size - ((ops.strokeCount - i) * ops.strokeSpacing), ops.size);
         ctx.stroke();
     }
 }
